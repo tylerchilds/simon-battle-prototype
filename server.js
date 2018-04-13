ac8e65ebbf88856da43690e02f51c8a3edda69ec
Tyler Childs <tylerwchilds@gmail.com>
2018-04-12 23:06:46 -0700

    sockets no conflict for prod

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import express from 'express';
+import { createServer } from 'http';
 import socket from 'socket.io';
 
 import GamesDatabase from './lib/GamesDatabase';
 
-const io = socket()
 const app = express();
+const server = createServer(app)
+const io = socket(server)
 
 const port = process.env.PORT ? process.env.PORT : 8181;
 const dist = path.join(__dirname, 'dist');
@@ -14,13 +16,6 @@ app.use(express.static(dist));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(dist, 'index.html'));
-});
-
-app.listen(port, (error) => {
-  if (error) {
-    console.log(error); // eslint-disable-line no-console
-  }
-  console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
 });
 
 let db = new GamesDatabase()
@@ -42,6 +37,9 @@ io.on('connection', function(client){
   });
 });
 
-
-io.listen(8000);
-console.log('listening on port ', 8000);
+server.listen(port, (error) => {
+  if (error) {
+    console.log(error); // eslint-disable-line no-console
+  }
+  console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
+});
--- src/lib/GameClient.js
+++ src/lib/GameClient.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 
 export default class GameClient{
   constructor(options){
-    this.socket = openSocket(":8000");
+    this.socket = openSocket();
     this.socket.on('initialize', options.initialized);
     this.socket.on('update', options.updated);
   }
